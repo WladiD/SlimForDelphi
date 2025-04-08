e1c9e302c249953a57d7b9359f3462415660f6ef
Waldemar Derr <wderr@taifun-software.de>
2025-04-08 12:39:10 +0200

    More tests and .gitignore

--- Test/Test.SlimList.pas
+++ Test/Test.SlimList.pas
@@ -48,6 +48,18 @@ begin
     Assert.IsNotNull(SlimList);
     Assert.AreEqual(34,SlimList.Count);
     Assert.IsTrue(SlimList.Entries[0] is TSlimListEntry);
+
+    var Entry1: TSlimListEntry := SlimList.Entries[0] as TSlimListEntry;
+    Assert.AreEqual(4, Entry1.List.Count);
+    Assert.AreEqual('decisionTable_0_0', (Entry1.List.Entries[0] as TSlimStringEntry).ToString);
+    Assert.AreEqual('make', (Entry1.List.Entries[1] as TSlimStringEntry).ToString);
+    Assert.AreEqual('decisionTable_0', (Entry1.List.Entries[2] as TSlimStringEntry).ToString);
+    Assert.AreEqual('eg.Division', (Entry1.List.Entries[3] as TSlimStringEntry).ToString);
+
+    var Entry2: TSlimListEntry := SlimList.Entries[1] as TSlimListEntry;
+    Assert.AreEqual(5, Entry2.List.Count);
+    var SubEntry2: TSlimListEntry:=Entry2.List.Entries[4] as TSlimListEntry;
+    Assert.AreEqual(7, SubEntry2.List.Count);
   finally
     Unserializer.Free;
   end;
