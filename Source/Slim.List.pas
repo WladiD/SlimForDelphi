de97d0a73972e76ab740870c2484b9a53d9f42d7
Waldemar Derr <wderr@taifun-software.de>
2025-04-13 17:10:51 +0200

    Short MIT copyright header added

--- Source/Slim.List.pas
+++ Source/Slim.List.pas
@@ -1,4 +1,10 @@
-﻿unit Slim.List;
+﻿// ======================================================================
+//  Copyright (c) 2025 Waldemar Derr. All rights reserved.
+//
+//  Licensed under the MIT license. See included LICENSE file for details.
+// ======================================================================
+
+unit Slim.List;
 
 interface
 
