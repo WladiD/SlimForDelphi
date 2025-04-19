45c97aa3cc67b2802552745a5a51ca0a97f67891
WladiD <furevest@gmail.com>
2025-04-19 16:22:05 +0200

    First complete working example

---
+++ Example/TwoMinuteExample/Main.pas
@@ -0,0 +1,119 @@
+﻿// ======================================================================
+// Copyright (c) 2025 Waldemar Derr. All rights reserved.
+//
+// Licensed under the MIT license. See included LICENSE file for details.
+// ======================================================================
+
+unit Main;
+
+interface
+
+uses
+
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.StdCtrls,
+
+  Slim.Fixture,
+  Slim.Server;
+
+type
+
+  TMainForm = class(TForm)
+    LogMemo: TMemo;
+  private
+    FSlimServer: TSlimServer;
+    class constructor Create;
+    procedure Log(const AMessage: String);
+    procedure ReadRequestHandler(const AValue: String);
+    procedure WriteResponseHandler(const AValue: String);
+  public
+    procedure AfterConstruction; override;
+  end;
+
+  [SlimFixture('Division', 'eg')]
+  TSlimDivisionFixture = class(TSlimFixture)
+  private
+    FNumerator: Double;
+    FDenominator: Double;
+  public
+    constructor Create;
+    procedure SetNumerator(ANumerator: Double);
+    procedure SetDenominator(ADenominator: Double);
+    function Quotient: Double;
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.dfm}
+
+{ TMainForm }
+
+class constructor TMainForm.Create;
+begin
+  RegisterSlimFixture(TSlimDivisionFixture);
+end;
+
+procedure TMainForm.AfterConstruction;
+begin
+  inherited;
+  FSlimServer := TSlimServer.Create(Self);
+  FSlimServer.DefaultPort := 9000;
+  FSlimServer.Active := True;
+  FSlimServer.OnReadRequest := ReadRequestHandler;
+  FSlimServer.OnWriteResponse := WriteResponseHandler;
+end;
+
+procedure TMainForm.Log(const AMessage: String);
+begin
+  TThread.Synchronize(nil,
+    procedure
+    begin
+      LogMemo.Lines.Add(TimeToStr(Now) + ' - ' + AMessage);
+      LogMemo.Lines.Add(sLineBreak + '---' + sLineBreak);
+    end);
+end;
+
+procedure TMainForm.ReadRequestHandler(const AValue: String);
+begin
+  Log('Request:' + sLineBreak + AValue);
+end;
+
+procedure TMainForm.WriteResponseHandler(const AValue: String);
+begin
+  Log('Response:' + sLineBreak + AValue);
+end;
+
+{ TSlimDivisionFixture }
+
+constructor TSlimDivisionFixture.Create;
+begin
+//  raise Exception.Create('I''m not in the mood today.');
+end;
+
+function TSlimDivisionFixture.Quotient: Double;
+begin
+  Result := FNumerator / FDenominator;
+end;
+
+procedure TSlimDivisionFixture.SetDenominator(ADenominator: Double);
+begin
+  FDenominator := ADenominator;
+end;
+
+procedure TSlimDivisionFixture.SetNumerator(ANumerator: Double);
+begin
+  FNumerator := ANumerator;
+end;
+
+end.
