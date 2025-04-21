6e6d9086c7313cf2369b0275700f23a790f418a9
WladiD <furevest@gmail.com>
2025-04-21 13:29:43 +0200

    Copyright header added

--- Example/MultiFormExample/Entry.pas
+++ Example/MultiFormExample/Entry.pas
@@ -1,4 +1,10 @@
-﻿unit Entry;
+﻿// ======================================================================
+// Copyright (c) 2025 Waldemar Derr. All rights reserved.
+//
+// Licensed under the MIT license. See included LICENSE file for details.
+// ======================================================================
+
+unit Entry;
 
 interface
 
