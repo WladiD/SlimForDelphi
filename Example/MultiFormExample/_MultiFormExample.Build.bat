7448ac4d92a36d13be18b40da0f00d602c0beaa6
WladiD <furevest@gmail.com>
2025-11-27 08:14:38 +0100

    Build batches for example projects

---
+++ Example/MultiFormExample/_MultiFormExample.Build.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal
+pushd %~dp0
+
+REM Static path to Delphi 12!
+set "RSVARS_PATH=C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
+
+for %%i in ("%RSVARS_PATH%\..\..") do set "PRODUCTVERSION=%%~nxi"
+
+if not defined PRODUCTVERSION (
+    echo ERROR: Could not determine PRODUCTVERSION from RSVARS_PATH.
+    popd
+    exit /b 1
+)
+
+echo Setting up Delphi environment...
+call "%RSVARS_PATH%"
+
+if %ERRORLEVEL% neq 0 (
+    echo ERROR: Failed to set up Delphi environment.
+    popd
+    exit /b %ERRORLEVEL%
+)
+
+echo.
+echo BDS environment variable is: "%BDS%"
+echo PRODUCTVERSION is: "%PRODUCTVERSION%"
+echo.
+
+echo Building MultiFormExample project...
+msbuild "MultiFormExample.dproj" /t:Build /p:Configuration=Platform=Win32;PRODUCTVERSION=%PRODUCTVERSION%;DCC_Define=DEBUG
+
+set BUILD_ERROR=%ERRORLEVEL%
+if %BUILD_ERROR% neq 0 (
+    echo ERROR: Failed to build the project.
+    popd
+    exit /b %BUILD_ERROR%
+)
+
+
+popd
+endlocal
