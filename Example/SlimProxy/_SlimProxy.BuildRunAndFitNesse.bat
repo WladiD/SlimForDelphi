3272884e876735b8d4b6179260ce1431ae33fa52
WladiD <furevest@gmail.com>
2025-12-01 16:14:25 +0100

    Batch adjusted for the fact, that the SlimProxy is started by the test page

--- Example/SlimProxy/_SlimProxy.BuildRunAndFitNesse.bat
+++ Example/SlimProxy/_SlimProxy.BuildRunAndFitNesse.bat
@@ -15,15 +15,13 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
-start Win32\Debug\SlimProxy.exe
-
 rem Navigate to the FitNesse directory relative to the project root
 cd ..\..\FitNesse
 
 rem Execute FitNesse test
 rem The command path "Playground.MultiFormExampleProxy" is relative to FitNesseRoot.
 rem The FitNesse executable is expected to be in the current directory.
-java -Dslim.port=9000 -Dslim.pool.size=1 -jar fitnesse-standalone.jar -c "Playground.MultiFormExampleProxy?test&format=text"
+java -jar fitnesse-standalone.jar -c "Playground.MultiFormExampleProxy?test&format=text"
 
 if %ERRORLEVEL% neq 0 (
     echo FitNesse test execution failed.
@@ -31,10 +29,14 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
+echo.
+powershell -Command "Write-Host -ForegroundColor Green -BackgroundColor Black 'FitNesse tests OK'"
+echo.
+
 popd
 endlocal
 
-pause 
+rem pause 
 
 rem Kill any existing instances of SlimProxy.exe and MultiFormExample.exe
 taskkill /IM SlimProxy.exe /F 2>nul
