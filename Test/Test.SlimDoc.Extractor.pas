f3d5cd199548fe0c805e498ad3ece9acf4855791
WladiD <furevest@gmail.com>
2025-12-21 07:27:21 +0100

    Formatting loop

--- Test/Test.SlimDoc.Extractor.pas
+++ Test/Test.SlimDoc.Extractor.pas
@@ -12,6 +12,7 @@ uses
 
   System.Classes,
   System.Generics.Collections,
+  System.SysUtils,
 
   DUnitX.TestFramework,
 
@@ -66,58 +67,62 @@ end;
 
 procedure TTestSlimDocExtractor.TestExtraction;
 var
+  Doc      : TSlimFixtureDoc;
   Extractor: TSlimDocExtractor;
-  Doc: TSlimFixtureDoc;
-  Method: TSlimMethodDoc;
-  Prop: TSlimPropertyDoc;
+  Method   : TSlimMethodDoc;
+  Prop     : TSlimPropertyDoc;
+
+  function GetDocMethod(const AName: String): TSlimMethodDoc;
+  begin
+    for var M in Doc.Methods do
+      if SameText(M.Name, AName) then
+        Exit(M);
+    Result := nil;
+  end;
+
 begin
+  Doc := nil;
   Extractor := TSlimDocExtractor.Create;
   try
     Doc := Extractor.ExtractClass(TSampleFixture);
-    try
-      Assert.AreEqual('TestFixture', Doc.Name);
-      Assert.AreEqual('test', Doc.Namespace);
-      Assert.AreEqual('TSampleFixture', Doc.DelphiClass);
+    Assert.AreEqual('TestFixture', Doc.Name);
+    Assert.AreEqual('test', Doc.Namespace);
+    Assert.AreEqual('TSampleFixture', Doc.DelphiClass);
 
-      // Check Methods (Should have Create, MethodOne, MethodTwo, SyncMethod)
-      if Doc.Methods.Count <> 4 then
-      begin
-        var Msg := 'Extracted methods: ';
-        for var M in Doc.Methods do Msg := Msg + M.Name + ', ';
-        Assert.AreEqual(4, Doc.Methods.Count, Msg);
-      end;
-      
-      // Look for specific method
-      Method := nil;
-      for var M in Doc.Methods do if M.Name = 'MethodTwo' then Method := M;
-      Assert.IsNotNull(Method);
-      Assert.AreEqual(1, Method.Parameters.Count);
-      Assert.AreEqual('A', Method.Parameters[0].Name);
-      Assert.AreEqual('Boolean', Method.ReturnType);
-
-      // Check SyncMode
-      Method := nil;
-      for var M in Doc.Methods do if M.Name = 'SyncMethod' then Method := M;
-      Assert.IsNotNull(Method);
-      Assert.AreEqual('smSynchronized', Method.SyncMode);
-
-      // Check constructor
-      Method := nil;
-      for var M in Doc.Methods do if M.Name = 'Create' then Method := M;
-      Assert.IsNotNull(Method);
-      Assert.AreEqual(1, Method.Parameters.Count);
-
-      // Check Property
-      Assert.AreEqual(1, Doc.Properties.Count);
-      Prop := Doc.Properties[0];
-      Assert.AreEqual('MyProp', Prop.Name);
-      Assert.AreEqual('Integer', Prop.PropertyType);
-      Assert.AreEqual('Read/Write', Prop.Access);
-
-    finally
-      Doc.Free;
+    // Check Methods (Should have Create, MethodOne, MethodTwo, SyncMethod)
+    if Doc.Methods.Count <> 4 then
+    begin
+      var Msg := 'Extracted methods: ';
+      for var M in Doc.Methods do
+        Msg := Msg + M.Name + ', ';
+      Assert.AreEqual(4, Doc.Methods.Count, Msg);
     end;
+
+    // Look for specific method
+    Method := GetDocMethod('MethodTwo');
+    Assert.IsNotNull(Method);
+    Assert.AreEqual(1, Method.Parameters.Count);
+    Assert.AreEqual('A', Method.Parameters[0].Name);
+    Assert.AreEqual('Boolean', Method.ReturnType);
+
+    // Check SyncMode
+    Method := GetDocMethod('SyncMethod');
+    Assert.IsNotNull(Method);
+    Assert.AreEqual('smSynchronized', Method.SyncMode);
+
+    // Check constructor
+    Method := GetDocMethod('Create');
+    Assert.IsNotNull(Method);
+    Assert.AreEqual(1, Method.Parameters.Count);
+
+    // Check Property
+    Assert.AreEqual(1, Doc.Properties.Count);
+    Prop := Doc.Properties[0];
+    Assert.AreEqual('MyProp', Prop.Name);
+    Assert.AreEqual('Integer', Prop.PropertyType);
+    Assert.AreEqual('Read/Write', Prop.Access);
   finally
+    Doc.Free;
     Extractor.Free;
   end;
 end;
